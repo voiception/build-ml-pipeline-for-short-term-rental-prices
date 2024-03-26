7c8e4ce60517404388a002c390118c7f4c6b5ec2
voiception <voiception@gmail.com>
2024-03-26 11:45:32 +0100

    Step 4: Initial Training - Test

--- main.py
+++ main.py
@@ -116,12 +116,15 @@ def go(config: DictConfig):
             )
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            # Test your production model against the test set
+    	    _ = mlflow.run(
+    	    	f"{config['main']['components_repository']}/test_regression_model",
+    	    	"main",
+            	parameters={
+                    "mlflow_model": "random_forest_export:prod",
+            	    "test_dataset": "test_data.csv:latest"
+                },    	    	
+    	    )
 
 
 if __name__ == "__main__":
